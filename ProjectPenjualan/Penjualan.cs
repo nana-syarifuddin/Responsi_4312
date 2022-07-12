b76ae2968ee6bf241c6bdf0d3fec3df3202fc454
nana-syarifuddin <nana.informatika@students.amikom.ac.id>
2022-07-12 16:21:55 +0700

    Responsi Pemrograman UAS 4312

---
+++ ProjectPenjualan/Penjualan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectPenjualan
+{
+    public class Penjualan
+    {
+        // PERINTAH: lengkapi property class penjualan, sesuai petunjuk soal
+
+        public string Nota { get; set; }
+        public string Tanggal { get; set; }
+        public string Nama { get; set; }
+        public string Jenis { get; set; }
+        public string Total { get; set; }
+    }
+}
